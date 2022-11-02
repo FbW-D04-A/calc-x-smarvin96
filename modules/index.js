4b80226eedea308860b7b88a1c0231a512ce9831
Marvin Sprenger <sprenger-marvin@outlook.de>
2022-11-02 16:02:24 +0100

    1+2

--- modules/index.js
+++ modules/index.js
@@ -1,6 +1,31 @@
 // Your code goes here!
+import { modulo, percentage, percentageOf, difference } from "/modules/percentage.js";
+import { calculateAspectRatio } from "/modules/aspect-ratio.js"
 
-import * as ar from "./aspect-ratio";
-import * as pt from "./percentage";
 
 
+const moduloForm = document.getElementById("modulo-form");
+moduloForm.addEventListener("change", (e) => {
+    const result = document.getElementById("modulo_result");
+    const input = [document.getElementById("modulo_1"), document.getElementById("modulo_2")]
+    switch (e.target.id) {
+        case "modulo_1":
+        case "modulo_2":
+            result.value = modulo(Number(input[0].value), Number(input[1].value));
+    }
+});
+
+
+
+// addEventListener change, 
+
+const percentageForm = document.getElementById("percentage-form");
+percentageForm.addEventListener("change", (e) => {
+    const result = document.getElementById("percentage_result");
+    const input = [document.getElementById("percentage_1"), document.getElementById("percentage_2")]
+    switch (e.target.id) {
+        case "percentage_1":
+        case "percentage_2":
+            result.value = percentage(Number(input[0].value), Number(input[1].value));
+    }
+});
